de38c63b153539acb9f3d6a549d8b129af8470cd
jerryho <jerryjp3914@gmail.com>
2023-11-10 15:31:26 +0800

    experiment: timeout processor(failure)

--- 5.param_comparison.py
+++ 5.param_comparison.py
@@ -2,6 +2,8 @@ import openai
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import concurrent.futures
+import time
 
 load_dotenv()  # take environment variables from .env.
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -20,12 +22,23 @@ def get_response(prompt, temperature, top_p):
     )
     return response.choices[0].message['content']
 
-prompts = ["Write a tagline for an ice cream shop."]
-temperatures = [0.7, 1.0, 1.7]  # Example temperature values
-top_ps = [0.2, 0.8, 0.95]       # Example top_p values
-num_requests = 10  # Number of requests per combination
+# Define a wrapper function that includes a timeout
+def get_response_with_timeout(prompt, temperature, top_p, timeout=2):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(get_response, prompt, temperature, top_p) 
+        try:
+            return future.result(timeout=timeout)  # Enforce the 3 seconds timeout
+        except concurrent.futures.TimeoutError:
+            return f"No response from server"
 
-csv_file_path = 'generated_responses.csv'
+prompts = ["Write a tagline for an ice cream shop."]
+# temperatures = [0.3, 1.7]  # Example temperature values
+# top_ps = [0.25, 0.35, 0.45, 0.85, 0.95 ]       # Example top_p values
+temperatures = [1.7]
+top_ps = [ 0.4 ] 
+num_requests = 8  # Number of requests per combination
+
+csv_file_path = 'generated_responses_2.csv'
 with open(csv_file_path, 'w', newline='') as csvfile:
     fieldnames = ['prompt', 'temperature', 'top_p', 'response']
     writer = pd.DataFrame(columns=fieldnames)
@@ -38,7 +51,7 @@ with open(csv_file_path, 'w', newline='') as csvfile:
         for temperature in temperatures:
             for top_p in top_ps:
                 for _ in range(num_requests):
-                    response = get_response(prompt, temperature, top_p)
+                    response = get_response_with_timeout(prompt, temperature, top_p)
                     print(response)
                     writer = pd.DataFrame([{
                             'prompt': prompt,
@@ -49,4 +62,3 @@ with open(csv_file_path, 'w', newline='') as csvfile:
                     writer.to_csv(csvfile, header=False, index=False)
 
 csv_file_path
-
